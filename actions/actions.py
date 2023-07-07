55c810c2b157296c88cb13006b441b72b4f6ac18
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-07-07 11:40:57 -0600

    Adds create cluster PoC

--- actions/actions.py
+++ actions/actions.py
@@ -4,14 +4,16 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List, Optional
 
 from rasa_sdk import Action, Tracker
+from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import ActionExecuted
+from rasa_sdk.events import ActionExecuted, EventType
 import requests
 from .auth import get_auth_token
 from ..common.identity import get_identity
+from .forms import IntentBasedFormValidationAction
 
 
 class ConsoleAPIAction(Action):
@@ -48,3 +50,75 @@ class ConsoleAPIAction(Action):
 
         events = [ActionExecuted(self.name())]
         return events
+
+
+class OpenshiftCreateClusterAction(IntentBasedFormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_form_openshift_clusters_create-cluster"
+
+    def utter_not_extracted(self, slot_name: Text) -> Optional[Text]:
+        return "Sorry. I did not understand your answer. Could you try again?"
+
+    def form_finished(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict):
+        slots = tracker.slots
+        is_managed_and_hosted = slots.get("openshift_managed") == "yes" and slots.get("openshift_hosted") == "hosted"
+        is_on_cloud = slots.get("openshift_where") == "cloud"
+        provider = slots.get("openshift_provider")
+
+        found = False
+
+        if is_managed_and_hosted and is_on_cloud:
+            if provider == "aws":
+                dispatcher.utter_message(text="Based on your answers, we think you should install using Red Hat "
+                                              "OpenShift Service on AWS (ROSA).")
+                found = True
+            elif provider == "azure":
+                dispatcher.utter_message(text="Based on your answers, we think you should install using Azure Red Hat "
+                                              "Openshift")
+                found = True
+
+        if not found:
+            dispatcher.utter_message(text="Based on your answers, we don't know what you should install - whoops.")
+
+        dispatcher.utter_message(text="Your answers:")
+        dispatcher.utter_message(text=f" - Where: {slots.get('openshift_where')}")
+        if is_on_cloud:
+            dispatcher.utter_message(text=f" - Provider: {slots.get('openshift_provider')}")
+        dispatcher.utter_message(text=f" - Managed by Red Hat: {slots.get('openshift_managed')}")
+        dispatcher.utter_message(text=f" - Hosted or Standalone control plane?: {slots.get('openshift_hosted')}")
+
+    async def extract_openshift_hosted(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict):
+        next_slot = (await self.next_requested_slot(dispatcher, tracker, domain)).get("value")
+        value = None
+
+        if next_slot == "openshift_hosted":
+            message = tracker.latest_message.get("text").lower()
+            # Todo: Replace with embeddings
+            if "standalone" in message:
+                value = "standalone"
+            elif "hosted" in message:
+                value = "hosted"
+
+        return {
+            "openshift_hosted": value
+        }
+
+    async def required_slots(
+            self,
+            domain_slots: List[Text],
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict
+    ) -> List[Text]:
+        base_slots = [
+            "openshift_where",
+            "openshift_provider",
+            "openshift_managed",
+            "openshift_hosted"
+        ]
+
+        if tracker.slots.get("openshift_where") != "cloud":
+            base_slots.remove("openshift_provider")
+
+        return base_slots
---
+++ actions/forms.py
@@ -0,0 +1,104 @@
+from abc import abstractmethod
+from typing import Any, Text, Dict, List, Optional
+
+from rasa_sdk import Tracker
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import EventType
+
+
+class IntentBasedFormValidationAction(FormValidationAction):
+
+    @abstractmethod
+    def name(self) -> Text:
+        """Unique identifier of this simple action."""
+        raise NotImplementedError("An action must implement a name")
+
+    async def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict
+    ) -> List[EventType]:
+        result = await super().run(dispatcher, tracker, domain)
+
+        form_finished = getattr(self, 'form_finished', None)
+        has_all_slots = (await self.next_requested_slot(dispatcher, tracker, domain)).get("value") is None
+        if form_finished is not None and has_all_slots:
+            form_finished(dispatcher, tracker, domain)
+
+        return result
+
+    def utter_not_extracted(self, slot_name: Text) -> Optional[Text]:
+        return None
+
+    """Returns the intent that started the active loop if there is any"""
+    def get_trigger_intent(self, tracker: Tracker) -> Text:
+        if tracker.active_loop:
+            return tracker.active_loop["trigger_message"]["intent"]["name"]
+
+        # No active-loop. Assume current intent is the one triggering this loop
+        return tracker.get_intent_of_latest_message()
+
+    async def get_extraction_events(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict
+    ) -> List[EventType]:
+        slots_to_extract = await self.required_slots(
+            self.domain_slots(domain), dispatcher, tracker, domain
+        )
+        next_slot = None
+
+        for slot in slots_to_extract:
+            if tracker.slots.get(slot) is None:
+                next_slot = slot
+                break
+
+        extraction_events = await super().get_extraction_events(dispatcher, tracker, domain)
+
+        if next_slot is not None and \
+                tracker.slots.get(next_slot) is None and \
+                tracker.get_intent_of_latest_message() != self.get_trigger_intent(tracker):
+            utterance = self.utter_not_extracted(next_slot)
+            if utterance is not None:
+                dispatcher.utter_message(text=utterance)
+
+        return extraction_events
+
+    def _generate_extract_slot(self, slot_name: Text):
+        async def extract_slot(dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict):
+            intent = tracker.get_intent_of_latest_message()
+            if intent == self.get_trigger_intent(tracker):
+                return {slot_name: tracker.slots.get(slot_name)}
+
+            expected_intent_start = f"intent_{slot_name}_"
+            previous_slot_value = tracker.slots.get(slot_name)
+            found_intent_value = None
+
+            if intent.startswith(expected_intent_start):
+                found_intent_value = intent[len(expected_intent_start):]
+
+            return {
+                slot_name: found_intent_value or previous_slot_value
+            }
+        return extract_slot
+
+    async def _extract_slot(
+            self,
+            slot_name: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        method_name = f"extract_{slot_name.replace('-', '_')}"
+
+        if not getattr(self, method_name, None):
+            mappings = domain.get("slots").get(slot_name).get("mappings")
+
+            if len(list(filter(lambda m: m.get("type") == "custom", mappings))) > 0:
+                setattr(self, method_name, self._generate_extract_slot(slot_name))
+
+        return await super()._extract_slot(slot_name, dispatcher,  tracker, domain)
