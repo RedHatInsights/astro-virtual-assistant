03bb2a6c91ad1f41ceb15f404512cab183cb17c4
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-09-22 10:38:04 -0600

    Add python formatter `black`

--- make/Makefile.lint.mk
+++ make/Makefile.lint.mk
@@ -3,13 +3,18 @@
 #
 
 PRETTIER_ARGS = -c
+BLACK_ARGS =
 
+lint: BLACK_ARGS += --check
 lint: --lint
 
 lint-fix: PRETTIER_ARGS +=  -w
 lint-fix: --lint
 
---lint: --lint-prettier
+--lint: --lint-prettier --lint-black
 
 --lint-prettier:
 	npx prettier "data/**/**.yml" $(PRETTIER_ARGS)
+
+--lint-black:
+	black . $(BLACK_ARGS)
