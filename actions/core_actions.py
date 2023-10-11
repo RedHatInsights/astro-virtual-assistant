98faa4490a8b4a0777c367add8f88df3355469c6
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-10-11 07:45:37 -0600

    RHCLOUD-28179 First time user flow

--- actions/core_actions.py
+++ actions/core_actions.py
@@ -3,7 +3,7 @@ from typing import Text, Dict, List, Any
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import Action
-from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.events import SlotSet, UserUtteranceReverted
 
 
 class ActionBack(Action):
@@ -16,3 +16,15 @@ class ActionBack(Action):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[Dict[Text, Any]]:
         return [UserUtteranceReverted()]
+
+
+class ActionFirstSessionStart(Action):
+    """Fired after `intent_core_session_start` the first time the session starts"""
+
+    def name(self) -> Text:
+        return "action_core_first_session_start"
+
+    async def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        return [SlotSet("first_time_greeting", False)]
