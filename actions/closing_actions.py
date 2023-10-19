14339e92008819a115606b874cee01ae56b8671f
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-10-19 06:22:26 -0600

    RHCLOUD-28181 Implement closing action (#50)
    * RHCLOUD-28181 Implement closing action
    
    * Replace print with a future TODO
    
    * - Remove checkpoints and use stories  starting with actions

---
+++ actions/closing_actions.py
@@ -0,0 +1,34 @@
+from typing import Text, Dict, List, Any
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import Action
+from rasa_sdk.events import SlotSet
+
+
+class ActionGotHelp(Action):
+
+    def name(self) -> Text:
+        return "action_closing_got_help"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        last_intent = tracker.get_intent_of_latest_message(True)
+        if last_intent == "intent_core_yes":
+            return [SlotSet("closing_got_help", True)]
+        elif last_intent == "intent_core_no":
+            return [SlotSet("closing_got_help", False)]
+
+        return []
+
+
+class ActionShareFeedback(Action):
+    def name(self) -> Text:
+        return "action_closing_share_feedback"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        # Todo: Insert logic to send feedback to the platform
+        return [SlotSet("closing_feedback", None)]
