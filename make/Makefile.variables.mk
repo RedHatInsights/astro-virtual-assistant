d57d72058859c75bf7bd625d918910ca89c3a890
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-10-24 13:28:27 -0600

    Allow to execute targets with --debug or --verbose flags (#64)
    VERBOSE=1 make train
     DEBUG=1 make run-cli

--- make/Makefile.variables.mk
+++ make/Makefile.variables.mk
@@ -6,3 +6,13 @@ RASA_ENDPOINTS_ARG = --endpoints endpoints.yml
 
 RASA_TRAIN_ARGS = ${RASA_DOMAIN_ARG}
 RASA_RUN_ARGS = ${RASA_ENDPOINTS_ARG}
+
+ifdef DEBUG
+  RASA_TRAIN_ARGS += --debug
+  RASA_RUN_ARGS += --debug
+endif
+
+ifdef VERBOSE
+  RASA_TRAIN_ARGS += --verbose
+  RASA_RUN_ARGS += --verbose
+endif
