0c5cd184b3aea020ac4ff1968545de1086da0402
Justin Orringer <justinorringer@gmail.com>
2023-10-27 11:28:59 -0400

    "id" should be "response"

--- actions/user_preferences_action.py
+++ actions/user_preferences_action.py
@@ -4,11 +4,8 @@ from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import Action
 
-import logging
-
 personal_info = ['email', 'email address', 'username', 'name', 'display name', 'address']
 
-logger = logging.getLogger(__name__)
 class ActionUserPreferences(Action):
 
     def name(self) -> Text:
@@ -17,8 +14,6 @@ class ActionUserPreferences(Action):
     async def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[Dict[Text, Any]]:
-        
-        logging.error("This is an error message")
 
         entities = tracker.latest_message['entities']
         preference = None
@@ -26,25 +21,19 @@ class ActionUserPreferences(Action):
             if e['entity'] == 'preference':
                 preference = e['value']
         
-        logging.error(preference)
         if preference in personal_info:
-            logging.error("personal info")
-            dispatcher.utter_message(text="You can change your " + preference + " and other personal information on redhat.com.")
+            dispatcher.utter_message(response="utter_user_preferences_specific", preference=preference)
         elif preference == 'password':
-            logging.error("password")
-            dispatcher.utter_message(id="utter_user_preferences_password")
-            dispatcher.utter_message(id="utter_user_preferences_change_password")
+            dispatcher.utter_message(response="utter_user_preferences_specific", preference="password")
+            dispatcher.utter_message(response="utter_user_preferences_change_password")
             return []
         elif preference == 'login':
-            logging.error("login")
-            dispatcher.utter_message(id="utter_user_preferences_login")
-            dispatcher.utter_message(id="utter_user_preferences_contact_support")
+            dispatcher.utter_message(response="utter_user_preferences_login")
+            dispatcher.utter_message(response="utter_user_preferences_contact_support")
             return []
         else:
-            logging.error("else")
-            dispatcher.utter_message(id="utter_user_preferences_all")
+            dispatcher.utter_message(response="utter_user_preferences_all")
 
-        logging.error("after if")
-        dispatcher.utter_message(id="utter_user_preferences_change_all")
+        dispatcher.utter_message(response="utter_user_preferences_change_all")
 
         return []
