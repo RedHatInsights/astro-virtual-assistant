21d8d25a583a7a538d615fcafbc90c31e406d24e
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-11-01 12:59:12 -0600

    Output all story errors instead of the first one

--- make/Makefile.test.mk
+++ make/Makefile.test.mk
@@ -6,9 +6,7 @@ include make/Makefile.variables.mk
 
 RASA_TEST_STORIES_PARAMS = --stories tests/stories ${RASA_DOMAIN_ARG}
 
-ifndef TEST_ALLOW_ERRORS
-	RASA_TEST_STORIES_PARAMS += --fail-on-prediction-errors
-endif
+__FAILED_TEST_STORIES = results/failed_test_stories.yml
 
 validate: test-data
 
@@ -25,6 +23,7 @@ test-nlu:
 
 test-stories:
 	${RASA_EXEC} test ${RASA_TEST_STORIES_PARAMS}
+	@(head -n 1 ${__FAILED_TEST_STORIES} | grep -q '# None of the test stories failed - all good!') || (cat ${__FAILED_TEST_STORIES} && false)
 
 test-python:
 	pipenv run pytest
