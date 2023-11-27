c63369ce44a326fa833c68187426279c96b72da1
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-11-27 12:52:06 -0700

    RHCLOUD-29509 Moving closing flow as a form

--- actions/closing_actions.py
+++ actions/closing_actions.py
@@ -1,38 +1,72 @@
 from typing import Text, Dict, List, Any
 
-from rasa_sdk import Tracker
+from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import Action
 from rasa_sdk.events import SlotSet
+from rasa_sdk.types import DomainDict
 
-from common import metrics
 
-
-class ActionGotHelp(Action):
+class ValidateFormClosing(FormValidationAction):
     def name(self) -> Text:
-        return "action_closing_got_help"
+        return "validate_form_closing"
 
     async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[Dict[Text, Any]]:
-        metrics.action_custom_action_count.labels(action_type=self.name()).inc()
+        if tracker.get_slot("requested_slot") == "closing_got_help":
+            closing_got_help = tracker.get_slot("closing_got_help")
+            if closing_got_help is True:
+                dispatcher.utter_message(response="utter_closing_got_help_yes")
+            elif closing_got_help is False:
+                dispatcher.utter_message(response="utter_closing_got_help_no")
 
-        last_intent = tracker.get_intent_of_latest_message(True)
-        if last_intent == "intent_core_yes":
-            return [SlotSet("closing_got_help", True)]
-        elif last_intent == "intent_core_no":
-            return [SlotSet("closing_got_help", False)]
+        return await super().run(dispatcher, tracker, domain)
 
-        return []
+    async def required_slots(
+        self,
+        domain_slots: List[Text],
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[Text]:
+        if tracker.get_slot("closing_leave_feedback") is False:
+            updated_slots = domain_slots.copy()
+            updated_slots.remove("closing_feedback")
+            return updated_slots
+
+        return domain_slots
 
 
-class ActionShareFeedback(Action):
+class ExecuteFormClosing(Action):
     def name(self) -> Text:
-        return "action_closing_share_feedback"
+        return "execute_form_closing"
 
     async def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> List[Dict[Text, Any]]:
-        metrics.action_custom_action_count.labels(action_type=self.name()).inc()
-        # Todo: Insert logic to send feedback to the platform
-        return [SlotSet("closing_feedback", None)]
+        # Todo: Insert logic to send feedback
+        if tracker.get_slot("closing_leave_feedback") is True:
+            dispatcher.utter_message(response="utter_closing_share_feedback")
+            print(f"Should send the following feedback:{tracker.get_slot('closing_feedback')}")
+
+        dispatcher.utter_message(response="utter_closing_finally")
+        return [SlotSet(key, None) for key in ["closing_got_help", "closing_leave_feedback", "closing_feedback"]]
+
+
+class ExecuteFormClosingAnythingElse(Action):
+    def name(self) -> Text:
+        return "execute_form_closing_anything_else"
+
+    async def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        closing_anything_else = tracker.get_slot("closing_anything_else")
+        if closing_anything_else is True:
+            dispatcher.utter_message(response="utter_core_how_can_i_help")
+        elif closing_anything_else is False:
+            dispatcher.utter_message(response="utter_closing_bye")
+        else:
+            return []
+
+        return [SlotSet(key, None) for key in ["closing_anything_else"]]
