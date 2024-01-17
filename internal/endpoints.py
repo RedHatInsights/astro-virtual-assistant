b011db11cbea94dd1414a28e7a4098c26175b112
Justin Orringer <justinorringer@gmail.com>
2024-01-17 14:02:35 -0500

    Internal API setup (#241)
    * Adding flask package
    
    * Choosing flask version >3.0.0
    
    * Setting up internal api
    
    * Working flask app
    
    * Dockerfile
    
    * Adding internal api to the deploy file
    
    * lint dangit
    
    * Separate internal api package category
    
    * Forgot to lock pipfile

---
+++ internal/endpoints.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+from common import logging
+from common.config import app
+
+
+flask_app = Flask(__name__)
+logger = logging.initialize_logging()
+
+
+def start_internal_api():
+    logger.info("Starting virtual assistant internal api...")
+    flask_app.run(host=app.hostname, port=app.internal_api_port)
+
+
+@flask_app.route("/", methods=["GET"])
+def health_check():
+    return jsonify({"status": "ok"})
