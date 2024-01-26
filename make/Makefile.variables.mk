be51027478fedb4be7314c75295a1fae8e4d3223
Josejulio Martínez <josejulio@users.noreply.github.com>
2024-01-26 13:32:47 -0700

    Use default rasa port as our defaults  - prevent adding --port when it is the default

--- make/Makefile.variables.mk
+++ make/Makefile.variables.mk
@@ -3,7 +3,6 @@
 PYTHON_EXEC = pipenv run python
 RASA_EXEC = ${PYTHON_EXEC} app.py
 RASA_ACTIONS_EXEC = ${PYTHON_EXEC} run_actions.py
-RASA_TRAIN_EXEC = pipenv run rasa
 RASA_DOMAIN_ARG = --domain data
 RASA_ENDPOINTS_ARG = --endpoints endpoints.yml
 
