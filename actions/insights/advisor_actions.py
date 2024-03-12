5ca5e0b91f8d321c3c39e531766bb8cc4103c06c
Josejulio Martínez <josejulio@users.noreply.github.com>
2024-03-12 09:03:50 -0600

    RHCLOUD-30551 to RHCLOUD-30554 Type of recommendations  (#325)
    * Add support for fuzzy matching with slots/extract
    
    * lint fix
    
    * RHCLOUD-30551 to RHCLOUD-30554 Type of recommendations
     - performance, stability, availability and security
    
    * lint fix
    
    ---------
    
    Co-authored-by: Justin Orringer <justinorringer@gmail.com>

--- actions/insights/advisor_actions.py
+++ actions/insights/advisor_actions.py
@@ -8,8 +8,9 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import ActionExecuted
+from rasa_sdk.events import ActionExecuted, UserUtteranceReverted
 
+from actions.slot_match import FuzzySlotMatch, FuzzySlotMatchOption, resolve_slot_match
 from common import logging
 from common.requests import send_console_request
 
@@ -92,3 +93,87 @@ class AdvisorAPIPathway(Action):
 
         events = [ActionExecuted(self.name())]
         return events
+
+
+CATEGORY_PERFORMANCE = "performance"
+CATEGORY_SECURITY = "security"
+CATEGORY_AVAILABILITY = "availability"
+CATEGORY_STABILITY = "stability"
+
+advisor_categories = FuzzySlotMatch(
+    "advisor_category",
+    [
+        FuzzySlotMatchOption(CATEGORY_PERFORMANCE),
+        FuzzySlotMatchOption(CATEGORY_SECURITY),
+        FuzzySlotMatchOption(CATEGORY_AVAILABILITY),
+        FuzzySlotMatchOption(CATEGORY_STABILITY),
+    ],
+)
+
+
+class AdvisorRecommendationByType(Action):
+
+    filter_query = "impacting=true&rule_status=enabled&sort=-total_risk"
+
+    def name(self) -> Text:
+        return "action_advisor_recommendation_by_type"
+
+    def cancel(self, dispatcher: CollectingDispatcher):
+        dispatcher.utter_message(response="utter_unknown_topic")
+        return [UserUtteranceReverted()]
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        user_input = tracker.latest_message["text"]
+
+        resolved = []
+        for word in user_input.split(" "):
+            resolved = resolve_slot_match(word, advisor_categories)
+
+            if len(resolved) > 0:
+                break
+
+        if len(resolved) == 0:
+            return self.cancel(dispatcher)
+
+        # Find the id of the category
+        response, content = await send_console_request(
+            "advisor", "/api/insights/v1/rulecategory/", tracker
+        )
+
+        if not response.ok:
+            return self.cancel(dispatcher)
+
+        category_id = None
+        category_name = None
+        for category in content:
+            if category["name"].lower() == resolved["advisor_category"]:
+                category_id = category["id"]
+                category_name = category["name"].lower()
+                break
+
+        if category_id is None:
+            return self.cancel(dispatcher)
+
+        response, content = await send_console_request(
+            "advisor",
+            f"/api/insights/v1/rule?category={category_id}&{self.filter_query}&limit=3",
+            tracker,
+        )
+
+        if not response.ok:
+            return self.cancel(dispatcher)
+
+        if len(content["data"]) > 0:
+            message = f"List of {category_name} recommendations\n"
+            index = 1
+            for rule in content["data"]:
+                message += f" {index}. [{rule['description']}](/insights/advisor/recommendations/{rule['rule_id']})\n"
+                index += 1
+
+            message += f"\n[See more {category_name} recommendations here](/insights/advisor/recommendations?category={category_id}&{self.filter_query}&limit=20&offset=0)."
+        else:
+            message = f"You don't have any {category_name} recommendation right now."
+
+        dispatcher.utter_message(text=message)
