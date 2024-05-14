4014063a2cad63c8043af6712ea0f1e7bd60f408
Tahmid Efaz <tefaz@redhat.com>
2024-05-14 11:34:41 -0400

    Fixing issues within the setup insights flow (#418)
    * fixing issues within setup insights flow
    
    * call slot cleanup action before the form
    
    * small test fix

--- actions/insights/general_setup.py
+++ actions/insights/general_setup.py
@@ -1,8 +1,9 @@
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
 
 from actions.actions import all_required_slots_are_set
 from actions.slot_match import FuzzySlotMatch, FuzzySlotMatchOption, resolve_slot_match
@@ -19,6 +20,18 @@ product_kind = FuzzySlotMatch(
         FuzzySlotMatchOption("openshift"),
     ],
 )
+
+
+class ActionSetupInsightsFormInit(Action):
+    def name(self) -> Text:
+        return "action_setup_insights_form_init"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        return [
+            SlotSet("insights_product_kind", None),
+        ]
 
 
 class InsightsGeneralSetup(FormValidationAction):
@@ -64,11 +77,18 @@ class InsightsGeneralSetup(FormValidationAction):
 
         if await all_required_slots_are_set(self, dispatcher, tracker, domain):
             slot_value = tracker.get_slot("insights_product_kind")
+
             if slot_value == "ansible":
-                dispatcher.utter_message(response="utter_ansible_activation_guide")
+                dispatcher.utter_message(
+                    response="utter_insights_general_ansible_activation_guide"
+                )
             elif slot_value == "openshift":
-                dispatcher.utter_message(response="utter_openshift_docs")
+                dispatcher.utter_message(
+                    response="utter_insights_general_openshift_docs"
+                )
             else:
-                dispatcher.utter_message(response="utter_rhel_config_guide")
+                dispatcher.utter_message(
+                    response="utter_insights_general_rhel_config_guide"
+                )
 
         return events
