153aba6d8f83f195e1fba4e75d91c8ff514970c6
Josejulio Martínez <josejulio@users.noreply.github.com>
2024-07-18 11:48:00 -0600

    Update internal-ui/src/services/messages.ts

--- internal-ui/src/services/messages.ts
+++ internal-ui/src/services/messages.ts
@@ -29,7 +29,6 @@ const getMessages = async (senderId?: string, cursor?: number): Promise<Array<Va
     throw new Error('Invalid request');
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getMessagesInRange = async (senderId?: string, start?: number, end?: number): Promise<Array<ValidMessage>> => {
     const request = senderId ? `/api/virtual-assistant/v1/messages/${senderId}` : `/api/virtual-assistant/v1/messages`;
     const sendRequest = async (start?: number, end?: number, offset?: number, limit?: number): Promise<RequestedMessageResponse> => {
