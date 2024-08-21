4c104f5f37f49d64c99ade573c3eb61f40ed0215
Tahmid Efaz <tefaz@redhat.com>
2024-08-21 10:28:02 -0400

    Subscription Flows (SU5, SU6, SU8) (#524)
    * adding flows su5,6,8
    
    * adding tests
    
    * lint 🍰
    
    * modifying utterances.

--- actions/insights/subscriptions_actions.py
+++ actions/insights/subscriptions_actions.py
@@ -1,14 +1,17 @@
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.types import DomainDict
 
 from actions.slot_match import FuzzySlotMatch, FuzzySlotMatchOption, resolve_slot_match
 from common import logging
 from common.requests import send_console_request
 
 logger = logging.initialize_logging()
+
+SUBS_PRODUCT_TYPE_SLOT = "subs_product_type"
 
 CATEGORY_ACTIVE = "active"
 CATEGORY_EXPIRED = "expired"
@@ -20,6 +23,14 @@ subs_categories = FuzzySlotMatch(
         FuzzySlotMatchOption(CATEGORY_ACTIVE),
         FuzzySlotMatchOption(CATEGORY_EXPIRED),
         FuzzySlotMatchOption(CATEGORY_EXPIRING),
+    ],
+)
+
+subs_product_type_categories = FuzzySlotMatch(
+    "subs_product_type",
+    [
+        FuzzySlotMatchOption("openshift", ["openshift", "open shift"]),
+        FuzzySlotMatchOption("rhel", ["rhel", "red hat enterprise linux", "linux"]),
     ],
 )
 
@@ -82,3 +93,51 @@ class SubscriptionsCheck(Action):
                     expiring=content_body["expiringSoon"],
                     expired=content_body["expired"],
                 )
+
+
+class SubsProductUsage(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_form_subs_product_usage"
+
+    @staticmethod
+    def extract_subs_product_type(
+        dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict
+    ) -> Dict[Text, Any]:
+        if tracker.get_slot("requested_slot") == SUBS_PRODUCT_TYPE_SLOT:
+            resolved = resolve_slot_match(
+                tracker.latest_message["text"], subs_product_type_categories
+            )
+
+            if len(resolved) > 0:
+                return resolved
+
+            return {SUBS_PRODUCT_TYPE_SLOT: None}
+
+        return {}
+
+    @staticmethod
+    def validate_subs_product_type(
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        return {SUBS_PRODUCT_TYPE_SLOT: value}
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        events = await super().run(dispatcher, tracker, domain)
+
+        requested_slot = tracker.get_slot("requested_slot")
+
+        if requested_slot == SUBS_PRODUCT_TYPE_SLOT:
+            product_type = tracker.get_slot(SUBS_PRODUCT_TYPE_SLOT)
+            if product_type == "rhel":
+                dispatcher.utter_message(response="utter_subs_product_usage_rhel_page")
+            elif product_type == "openshift":
+                dispatcher.utter_message(
+                    response="utter_subs_product_usage_openshift_page"
+                )
+
+        return events
