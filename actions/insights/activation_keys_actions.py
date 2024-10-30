638e2f04e314ed2ada4718cadec0ee3f8ae7ccb3
Justin Orringer <justinorringer@gmail.com>
2024-10-30 13:19:34 -0400

    Cleaning up and selecting status messages

--- actions/insights/activation_keys_actions.py
+++ actions/insights/activation_keys_actions.py
@@ -3,22 +3,16 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import (
-    ActionExecuted,
-    SlotSet,
-)
+from rasa_sdk.events import SlotSet
 from rasa_sdk.types import DomainDict
 
 from actions.actions import form_action_is_starting
-from actions.slot_match import FuzzySlotMatch, FuzzySlotMatchOption, resolve_slot_match
 
 from common import logging
-from common.header import Header
 from common.metrics import (
     flow_started_count,
     Flow,
     flow_finished_count,
-    action_custom_action_count,
 )
 from common.requests import send_console_request
 
@@ -42,18 +36,19 @@ class ValidateFormActivationKeyCreate(FormValidationAction):
         return {}
 
     @staticmethod
-    def validate_image_builder_rhel_version(
+    def validate_inventory_activation_key_name(
         value: Text,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         # must contain only numbers, letters, underscores, and hyphens.
+        print("value: ", value)
         pattern = r'^[a-zA-Z0-9_-]+$'
-        if not value or re.match(pattern, value):
-            dispatcher.utter_message(response="utter_inventory_activation_key_name_invalid")
-            return {ACTIVATION_KEY_NAME: None}
-        return {ACTIVATION_KEY_NAME: value}
+        if value and re.match(pattern, value):
+            return {ACTIVATION_KEY_NAME: value}
+        dispatcher.utter_message(response="utter_inventory_activation_key_name_invalid")
+        return {ACTIVATION_KEY_NAME: None}
 
     async def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
@@ -65,20 +60,28 @@ class ValidateFormActivationKeyCreate(FormValidationAction):
         requested_slot = tracker.get_slot("requested_slot")
 
         name = tracker.get_slot(ACTIVATION_KEY_NAME)
-        if requested_slot == ACTIVATION_KEY_NAME:
+        if requested_slot == ACTIVATION_KEY_NAME and name:
             # post to rhsm 	/api/rhsm/v2/activation_keys
             body = {"name": name, "role": "", "serviceLevel": "", "usage": ""}
             response = await send_console_request(
                 "rhsm", "/api/rhsm/v2/activation_keys", tracker, method="post", json=body, fetch_content=False
             )
 
-            if response is None or response.status_code != 200: 
+            if not response.ok: 
                 # error json format: {"error":{"code":400,"message":"name should be present, unique and only contain letters, numbers, underscores, or hyphens"}}
-                error_message = response.json().get("error", {}).get("message", "")
+                error_json = await response.json()
+                error_message = error_json.get("error", {}).get("message", "Unknown error")
                 dispatcher.utter_message(response="utter_inventory_create_activation_key_failure_1")
                 dispatcher.utter_message(response="utter_inventory_create_activation_key_failure_2", error_message=error_message)
+                logger.error(
+                    "Failed to get a response from the rhsm API: status {}; result {}".format(
+                        response.status, error_message
+                    ),
+                    exc_info=True,
+                )
+                flow_finished_count(Flow.ACTIVATION_KEY_CREATE, sub_flow_name="error")
             else:
-                dispatcher.utter_message(response="utter_inventory_activation_key_create_success")
+                dispatcher.utter_message(response="utter_inventory_create_activation_key_success")
                 flow_finished_count(Flow.ACTIVATION_KEY_CREATE)
 
         return events
@@ -91,3 +94,14 @@ class ValidateFormActivationKeyCreate(FormValidationAction):
         domain: DomainDict,
     ) -> List[Text]:
         return domain_slots
+
+class ActionActivationKeysClear(Action):
+    def name(self) -> Text:
+        return "action_inventory_create_activation_key_clear"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
+        return [
+            SlotSet(ACTIVATION_KEY_NAME, None),
+        ]
