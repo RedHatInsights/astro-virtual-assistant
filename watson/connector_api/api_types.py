ac77a6e1a4ded2b73b650fc5d28a6154a7a15a73
Tahmid Efaz <tefaz@redhat.com>
2025-01-23 16:37:17 -0500

    Adding very basic connector-api (#569)
    * adding very basic connector-api
    
    * moving the watson api packages to use python 3.9 for now
    
    * lint :sparkles:
    
    * reverting changes to Pipfile.lock

---
+++ watson/connector_api/api_types.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class TalkRequestMetadata(BaseModel):
+    session_id: Optional[str] = None
+    # account: str
+    # org_id: str
+
+
+class TalkRequest(BaseModel):
+    message: str
+    metadata: TalkRequestMetadata
