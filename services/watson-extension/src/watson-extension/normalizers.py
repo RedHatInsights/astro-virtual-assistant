790991b4938707184c03a3536cd096def04a28d7
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-01-29 15:06:26 -0700

    RHCLOUD-37500 Configure watson-extension to serve a status endpoint (#574)
    * RHCLOUD-37500 Configure watson-extension to serve a status endpoint
    
    * Refactoring app fixture to a s ingle function
    lint
    
    * - Installing --dev by default
    - Ignoring TestClientProtocol from being collected as a test
    - Adding pytest to dev of watson-extension
    
    * install all

--- services/watson-extension/src/watson-extension/normalizers.py
+++
@@ -1,4 +0,0 @@
-# Sample normalizer to format data that will come in from console_request
-def cat_facts_normalizer(data):
-    cat_fact = data.get("fact")
-    return {"cat_fact": cat_fact}
---
+++ services/watson-extension/tests/routes/test_health.py
@@ -0,0 +1,17 @@
+import pytest
+from quart.typing import TestClientProtocol
+from common import app_with_blueprint
+
+from routes.health import blueprint
+
+
+@pytest.fixture
+async def test_client() -> TestClientProtocol:
+    return app_with_blueprint(blueprint).test_client()
+
+
+async def test_status(test_client) -> None:
+    response = await test_client.get("/health/status")
+    assert response.status == "200 OK"
+    data = await response.get_json()
+    assert data == {"status": "ok"}
