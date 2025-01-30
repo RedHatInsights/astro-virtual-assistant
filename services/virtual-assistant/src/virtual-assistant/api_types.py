b0dcc54c6f7ce7096ed317a859b4ed8768bbecaf
Tahmid Efaz <tefaz@redhat.com>
2025-01-30 17:51:15 -0500

    supporting different watson message types; updating api interface

--- services/virtual-assistant/src/virtual-assistant/api_types.py
+++ services/virtual-assistant/src/virtual-assistant/api_types.py
@@ -1,13 +1,77 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
-class TalkRequestMetadata(BaseModel):
-    session_id: Optional[str] = None
-    # account: str
-    # org_id: str
+class TalkRequestInputIntents(BaseModel):
+    intent: str
+    confidence: Optional[float] = None
+    skill: Optional[str] = None
+
+
+class TalkRequestInputEntities(BaseModel):
+    entity: str
+    location: Optional[List[int]] = []
+    value: str
+    confidence: Optional[float] = None
+    groups: Optional[dict] = {}
+    interpretation: Optional[dict] = {}
+    alternatives: Optional[dict] = {}
+    role: Optional[dict] = {}
+    skill: Optional[str] = {}
+
+
+class TalkRequestInputAttachments(BaseModel):
+    url: str
+    media_type: Optional[str] = None
+
+
+class TalkRequestInputAnalytics(BaseModel):
+    browser: Optional[str] = None
+    device: Optional[str] = None
+    page_url: Optional[str] = None
+
+
+class TalkInput(BaseModel):
+    # TODO: update to support empty list `[]` for intents and entities key
+    message_type: Optional[str] = None
+    text: Optional[str] = None
+    intents: Optional[List[TalkRequestInputIntents]] = None
+    entities: Optional[List[TalkRequestInputEntities]] = None
+    suggestion_id: Optional[str] = None
+    attachments: Optional[TalkRequestInputAttachments] = None
+    analytics: Optional[TalkRequestInputAnalytics] = None
+    options: Optional[dict] = None
 
 
 class TalkRequest(BaseModel):
-    message: str
-    metadata: TalkRequestMetadata
+    session_id: Optional[str] = None
+    input: Optional[TalkInput] = None
+
+
+class ApiResponse(BaseModel):
+    text: str
+
+
+def watson_response_formatter(session_id: str, response: dict):
+    watson_generic = response["output"]["generic"]
+    normalized_watson_response = []
+
+    for generic in watson_generic:
+        print("generic", generic)
+        if generic["response_type"] == "text":
+            normalized_watson_response.append({"text": generic["text"]})
+
+        if generic["response_type"] == "option":
+            options = []
+            for option in generic["options"]:
+                options.append({"label": option["label"], "input": option["value"].get("input")})
+            normalized_watson_response.append({"options": options})
+
+        if generic["response_type"] == "suggestion":
+            options = []
+            for suggestion in generic["suggestions"]:
+                options.append({"label": suggestion["label"], "input": suggestion["value"].get("input")})
+            normalized_watson_response.append({"text": generic["title"], "options": options})
+
+    normalized_response = {"session_id": session_id, "response": normalized_watson_response}
+    return normalized_response
--- services/virtual-assistant/src/virtual-assistant/routes.py
+++ services/virtual-assistant/src/virtual-assistant/routes.py
@@ -6,7 +6,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from common.config import app
 
 from common.auth import require_identity_header, get_org_id_from_identity
-from api_types import TalkRequest
+from api_types import TalkRequest, watson_response_formatter
 
 
 api_blueprint = Blueprint("api", __name__)
@@ -31,18 +31,18 @@ async def talk():
     identity = request.headers.get("x-rh-identity")
     org_id = get_org_id_from_identity(identity)
 
-    if not data:
+    if not request_body:
         return jsonify({"error": "No request body"}), 400
 
     try:
-        validated_data = TalkRequest(**data)
+        validated_request_body = TalkRequest(**request_body)
     except ValidationError as e:
         return jsonify({"errors": e.errors()}), 400
 
     assistant = get_watson_assistant()
 
     # Create session if not provided
-    session_id = validated_data.metadata.session_id
+    session_id = validated_request_body.session_id
     if not session_id:
         watson_session_response = assistant.create_session(
             assistant_id=app.watson_env_id
@@ -50,15 +50,20 @@ async def talk():
         session_id = watson_session_response["session_id"]
 
     print("session_id", session_id)
+    watson_input = validated_request_body.input.dict(exclude_none=True)
+    watson_input["message_type"] = "text"
+    print("watson_input", watson_input)
+
     # Send message to Watson Assistant
-    user_message = validated_data.message
     watson_message_response = assistant.message(
         assistant_id=app.watson_env_id,
         environment_id=app.watson_env_id,
         session_id=session_id,
         user_id=org_id,  # using org_id and user_id to identity unique users
-        input={"message_type": "text", "text": user_message},
+        input=validated_request_body.input.dict(exclude_none=True),
     ).get_result()
 
+    response = watson_response_formatter(session_id, watson_message_response)
     ## reformat watson message response to include session id and other things
-    return jsonify(watson_message_response), 200
+    return jsonify(response), 200
+
