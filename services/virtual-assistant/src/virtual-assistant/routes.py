a726280c15f77f1cc27ee04040aa5a7bde30d410
Tahmid Efaz <tefaz@redhat.com>
2025-02-04 11:45:10 -0500

    simplifying the api interface, refactoring, and making endpoint more testable (#579)
    * simplifying api interface, refactoring, and making endpoint more testable
    
    * moving api types back to the virtual-assistant package

--- services/virtual-assistant/src/virtual-assistant/routes.py
+++ services/virtual-assistant/src/virtual-assistant/routes.py
@@ -1,27 +1,23 @@
 from quart import Blueprint, jsonify, request
 from quart_schema import validate_request, validate_response
-from ibm_watson import AssistantV2
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core.api_exception import ApiException
 
 from common.config import app
+from common.logging import initialize_logging
 from common.auth import require_identity_header, get_org_id_from_identity
+from common.types.errors import ValidationError
 from api_types import (
     TalkRequest,
     TalkResponse,
-    TalkRequestError,
-    watson_response_formatter,
 )
+from watson import WatsonAssistant
 
+
+logger = initialize_logging()
 
 api_blueprint = Blueprint("api", __name__)
 
-
-def get_watson_assistant():
-    authenticator = IAMAuthenticator(app.watson_api_key)
-    assistant = AssistantV2(version=app.watson_env_version, authenticator=authenticator)
-    assistant.set_service_url(app.watson_api_url)
-    return assistant
+assistant = WatsonAssistant()
 
 
 @api_blueprint.route("/", methods=["GET"])
@@ -33,38 +29,27 @@ async def health():
 @require_identity_header
 @validate_request(TalkRequest)
 @validate_response(TalkResponse, 200)
-@validate_response(TalkRequestError, 400)
+@validate_response(ValidationError, 400)
 async def talk(data: TalkRequest) -> TalkResponse:
     identity = request.headers.get("x-rh-identity")
     org_id = get_org_id_from_identity(identity)
 
-    assistant = get_watson_assistant()
+    assistant.authenticate()
 
-    # Create session if not provided
-    session_id = data.session_id
-    if not session_id:
-        watson_session_response = assistant.create_session(
-            assistant_id=app.watson_env_id
-        ).get_result()
-        session_id = watson_session_response["session_id"]
-
-    print("session_id", session_id)
-    watson_input = data.input.dict(exclude_none=True)
-    watson_input["message_type"] = "text"
+    session_id = assistant.create_session(data.session_id)
+    logger.info(f"session_id: {session_id}")
 
     # Send message to Watson Assistant
     watson_message_response = None
     try:
-        watson_message_response = assistant.message(
-            assistant_id=app.watson_env_id,
-            environment_id=app.watson_env_id,
+        watson_message_response = assistant.send_watson_message(
             session_id=session_id,
-            user_id=org_id,  # using org_id and user_id to identity unique users
+            org_id=org_id,  # using org_id as user_id to identity unique users
             input=data.input.dict(exclude_none=True),
-        ).get_result()
+        )
     except ApiException as e:
-        return TalkRequestError(message=e.message), 400
+        return ValidationError(message=e.message), 400
 
-    response = watson_response_formatter(session_id, watson_message_response)
+    response = assistant.format_response(session_id, watson_message_response)
 
     return response, 200
--- services/virtual-assistant/src/virtual-assistant/run.py
+++ services/virtual-assistant/src/virtual-assistant/run.py
@@ -1,10 +1,12 @@
 from quart import Quart
 from common.config import app
+from common.logging import initialize_logging
 from quart_schema import QuartSchema, RequestSchemaValidationError
 
 from routes import api_blueprint
-from api_types import TalkRequestError
+from common.types.errors import ValidationError
 
+logger = initialize_logging()
 
 api = Quart(__name__)
 
@@ -14,12 +16,13 @@ api.register_blueprint(api_blueprint, url_prefix=base_url)
 
 @api.errorhandler(RequestSchemaValidationError)
 async def handle_request_validation_error(error):
-    return TalkRequestError(message=str(error.validation_error)), 400
+    return ValidationError(message=str(error.validation_error)), 400
 
 
 QuartSchema(api, openapi_path=base_url + "/openapi.json")
 
 if __name__ == "__main__":
     port = int(app.connector_api_port)
-    print(f"Starting app at port {port} with base URL: {base_url}")
+
+    logger.info(f"Starting app at port {port} with base URL: {base_url}")
     api.run(port=port)
