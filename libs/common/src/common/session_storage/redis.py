7ce6076f7100efaecdbdf4cb047273e424f87be9
Tahmid Efaz <tefaz@redhat.com>
2025-02-07 15:42:44 -0500

    propagating errors and adding redis to the common package

--- libs/common/src/common/session_storage/redis.py
+++ libs/common/src/common/session_storage/redis.py
@@ -21,21 +21,14 @@ class RedisSessionStorage(SessionStorage):
 
     async def retrieve(self, session_key: str) -> Optional[Session]:    
         """Read the identity header from Redis using the session_id"""
-        try:
-            value = await self.redis_client.get(session_key)
-            if value:
-                return value
-            else:
-                return None
-        except Exception as e:
-            print(f"Error reading from Redis: {e}")
+        value = await self.redis_client.get(session_key)
+        if value:
+            return value
+        else:
             return None
 
     async def store(self, session: Session):
         """Write the session_id/identity header pair to Redis."""
-        try:
-            await self.redis_client.set(session.key, session.value)
-            print(f"Session ({session.key} written to Redis.")
-        except Exception as e:
-            print(f"Error writing to Redis: {e}")
+        await self.redis_client.set(session.key, session.value)
+        print(f"Session ({session.key} written to Redis.")
 
