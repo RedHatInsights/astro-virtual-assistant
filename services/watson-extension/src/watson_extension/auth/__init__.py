141e70a5fa7dd3f613ef2d914bf937830a212a88
Josejulio Martínez <jmartine@redhat.com>
2025-02-14 12:30:57 -0700

    self review

--- services/watson-extension/src/watson_extension/auth/__init__.py
+++ services/watson-extension/src/watson_extension/auth/__init__.py
@@ -1,18 +1,8 @@
-# import functools
 from abc import ABC, abstractmethod
 
-# import injector
 import quart
 
 
 class Authentication(ABC):
     @abstractmethod
     async def check_auth(self, request: quart.Request): ...
-
-# def require_auth(f):
-#     @functools.wraps(f)
-#     async def wrapper(authentication: injector.Inject[Authentication], *args, **kwargs):
-#         await authentication.check_auth(quart.request)
-#         f(*args, **kwargs)
-#
-#     return wrapper
