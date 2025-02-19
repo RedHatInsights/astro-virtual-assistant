6482a30f316ef00fee6a7a367286cdb784dd114e
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-02-19 12:39:52 -0700

    Remove unused functions (#602)
    * Remove unused functions
    
    * lint

--- libs/common/tests/common/test_config.py
+++ libs/common/tests/common/test_config.py
@@ -1,7 +1,7 @@
-import builtins
 import os
 from unittest import mock
 import sys
+from unittest.mock import MagicMock
 
 from .. import path_to_resource
 import pytest
@@ -31,6 +31,33 @@ def test_clowdapp_public_endpoints():
     from common.config import config
     assert config("ENDPOINT__ADVISOR_BACKEND__API__URL") == "http://n-api.svc:8000"
     assert config("ENDPOINT__NOTIFICATIONS_GW__SERVICE__URL") == "http://n-gw.svc:1337"
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "ACG_CONFIG": path_to_resource("clowdapp.json"),
+        "__DOT_ENV_FILE": ".i-dont-exist",
+    },
+    clear=True,
+)
+def test_clowdapp_public_endpoints_not_found():
+    from common.config import config
+    with pytest.raises(UndefinedValueError):
+        assert config("ENDPOINT__ADVISOR_BACKEND__COOL__URL") == "http://n-api.svc:8000"
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "ACG_CONFIG": path_to_resource("clowdapp.json"),
+        "__DOT_ENV_FILE": ".i-dont-exist",
+    },
+    clear=True,
+)
+def test_clowdapp_public_endpoints_invalid():
+    from common.config import config
+    with pytest.raises(ValueError):
+        assert config("ENDPOINT__ADVISOR_BACKEND__API__PORT") == "http://n-api.svc:8000"
 
 @mock.patch.dict(
     os.environ,
@@ -98,6 +125,8 @@ def test_redis():
     assert config("REDIS_PORT") == 99137
     assert config("REDIS_USERNAME") == "my-username"
     assert config("REDIS_PASSWORD") == "my-s3cret"
+    with pytest.raises(ValueError):
+        config("REDIS_DOMINATION_PLANS")
 
 @mock.patch.dict(
     os.environ,
@@ -142,12 +171,63 @@ def test_with_db_ca():
     clear=True,
 )
 def test_openshift_namespace():
-    def openshift_mock_open(*args, **kwargs):
-        if args[0] == "/var/run/secrets/kubernetes.io/serviceaccount/namespace":
-            return mock.mock_open(read_data="my-cool-namespace")(*args, **kwargs)
-
-        return builtins.open(*args, **kwargs)
-
-    with mock.patch("builtins.open", openshift_mock_open):
+    with mock.patch("builtins.open", MagicMock(return_value=mock.mock_open(read_data="my-cool-namespace"))()) as mocked_open:
         from common.config import config
         assert config("NAMESPACE") == "my-cool-namespace"
+        mocked_open.assert_called_once_with( "/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r")
+
+
+def test_openshift_namespace_has_cache():
+    with mock.patch("builtins.open", MagicMock(return_value=mock.mock_open(read_data="my-cool-namespace"))()) as mocked_open:
+        from common.config import config
+        assert config("NAMESPACE") == "my-cool-namespace"
+        assert config("NAMESPACE") == "my-cool-namespace"
+        assert config("NAMESPACE") == "my-cool-namespace"
+        mocked_open.assert_called_once_with( "/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r")
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "__DOT_ENV_FILE": ".i-dont-exist",
+        "I_AM_AN_ENV_VARIABLE": "yes"
+    },
+    clear=True,
+)
+def test_defaults_to_env():
+    from common.config import config
+    assert config("I_AM_AN_ENV_VARIABLE") == "yes"
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "ONE": "1",
+        "TWO": "two",
+        "true": "true",
+    }
+)
+def test_logger():
+    from common.config import config, log_config
+    mlogger = MagicMock()
+    def module(): ...
+    module.__dict__ = {
+        "one": config("ONE"),
+        "__private_one": config("ONE"),
+        "oneToken": config("ONE"),
+        "onePasswordStuff": config("ONE"),
+        "oneKeyEtc": config("ONE"),
+        "secretOfMonkeyIsland": config("ONE"),
+        "two": config("TWO"),
+        "myBool": config("true"),
+    }
+
+    with mock.patch("logging.getLogger", lambda _: mlogger):
+        log_config(module)
+        assert mlogger.info.call_count == 7
+        mlogger.info.assert_any_call("Using one: 1")
+        mlogger.info.assert_any_call("Using oneToken: *********")
+        mlogger.info.assert_any_call("Using onePasswordStuff: *********")
+        mlogger.info.assert_any_call("Using oneKeyEtc: *********")
+        mlogger.info.assert_any_call("Using secretOfMonkeyIsland: *********")
+        mlogger.info.assert_any_call("Using two: two")
+        mlogger.info.assert_any_call("Using myBool: true")
