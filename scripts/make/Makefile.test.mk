8725cdd9a032512e0aed4572095a50bdebe50e0b
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-02-19 09:18:28 -0700

    Start reporting coverage to codecov (#600)
    * Start reporting coverage to codecov
    
    * Write xml coverage reports
    
    * Specify coverage format
    
    * Also reporting test runtimes/failures

--- scripts/make/Makefile.test.mk
+++ scripts/make/Makefile.test.mk
@@ -11,5 +11,15 @@ test-python:
 	uv run --directory services/virtual-assistant pytest
 	uv run --directory services/watson-extension pytest
 
+test-python-coverage:
+	uv run --directory libs/common coverage run -m pytest --junitxml=junit.xml -o junit_family=legacy && uv run --directory libs/common coverage report
+	uv run --directory services/virtual-assistant coverage run -m pytest --junitxml=junit.xml && uv run --directory services/virtual-assistant coverage report
+	uv run --directory services/watson-extension coverage run -m pytest --junitxml=junit.xml && uv run --directory services/watson-extension coverage report
+ifneq ($(COVERAGE_FORMAT),)
+	uv run --directory libs/common coverage $(COVERAGE_FORMAT)
+	uv run --directory services/virtual-assistant coverage $(COVERAGE_FORMAT)
+	uv run --directory services/watson-extension coverage $(COVERAGE_FORMAT)
+endif
+
 test-openapi:
 	curl -X GET http://0.0.0.0:5005/api/virtual-assistant/v1/openapi.json
