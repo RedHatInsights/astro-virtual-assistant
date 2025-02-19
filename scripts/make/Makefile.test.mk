e65147a3ef53ca48b73e89da5671ccb2d4da3e77
Josejulio Martínez <jmartine@redhat.com>
2025-02-19 08:56:02 -0700

    Write xml coverage reports

--- scripts/make/Makefile.test.mk
+++ scripts/make/Makefile.test.mk
@@ -15,6 +15,11 @@ test-python-coverage:
 	uv run --directory libs/common coverage run -m pytest && uv run --directory libs/common coverage report
 	uv run --directory services/virtual-assistant coverage run -m pytest && uv run --directory services/virtual-assistant coverage report
 	uv run --directory services/watson-extension coverage run -m pytest && uv run --directory services/watson-extension coverage report
+ifneq ($(COVERAGE_FORMAT),)
+	uv run --directory libs/common coverage $(COVERAGE_FORMAT)
+	uv run --directory services/virtual-assistant coverage $(COVERAGE_FORMAT)
+	uv run --directory services/watson-extension coverage $(COVERAGE_FORMAT)
+endif
 
 test-openapi:
 	curl -X GET http://0.0.0.0:5005/api/virtual-assistant/v1/openapi.json
