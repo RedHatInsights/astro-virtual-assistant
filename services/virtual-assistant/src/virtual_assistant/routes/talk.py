485ab076532ca2306103f6d593173f1d62c9ca9f
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-02-20 14:03:39 -0700

    RHCLOUD-37477 Update clowdapp (#605)

--- services/virtual-assistant/src/virtual_assistant/routes/talk.py
+++ services/virtual-assistant/src/virtual_assistant/routes/talk.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 @validate_response(ValidationError, 400)
 async def talk(data: TalkRequest, assistant: injector.Inject[WatsonAssistant]) -> TalkResponse:
     identity = request.headers.get("x-rh-identity")
+    # Todo: Update to use user_id - org-id applies to multiple users
     org_id = get_org_id_from_identity(identity)
 
     # Todo: Get this from redis?
