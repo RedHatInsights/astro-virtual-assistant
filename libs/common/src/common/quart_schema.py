d995664ac35ff33720afc74a1c83fc5b988c4a2f
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-02-25 09:20:04 -0700

    Adds more details to openapi.json (#609)
    * Adds more details to openapi.json
    
    * Adjusting server url and removing not required stuff

--- libs/common/src/common/quart_schema.py
+++ libs/common/src/common/quart_schema.py
@@ -1,10 +1,15 @@
-from typing import Iterable
+from typing import Iterable, Dict, Any
 
 from quart_schema import OpenAPIProvider
 from quart_schema.openapi import Rule
 
 
 class VirtualAssistantOpenAPIProvider(OpenAPIProvider):
+    def schema(self) -> Dict[str, Any]:
+        schema = super().schema()
+        schema["openapi"] = "3.0.0"
+        return schema
+
     def generate_rules(self) -> Iterable[Rule]:
         for rule in super().generate_rules():
             # This static endpoints gets added when using quart-injector - unsure if it's a dev only rule, but hiding from here.
