f07305b1351fccf6e587bb6c7eab32061f6e6291
Josejulio Martínez <jmartine@redhat.com>
2025-02-26 14:16:52 -0700

    Revert "Implement a basic internal watson extension api (#570)"
    This reverts commit 81988b2fa544204d5cf416ed1729888cf703666e.

--- common/config/app.py
+++ common/config/app.py
@@ -134,11 +134,6 @@ connector_api_base_url = _config(
 )
 connector_api_port = _config("CONNECTOR_API_PORT", default=5000)
 
-internal_watson_extension_base_url = _config(
-    "INTERNAL_WATSON_EXTENSION_BASE_URL", default="/internal/api/v1"
-)
-internal_watson_extension_port = _config("INTERNAL_WATSON_EXTENSION_PORT", default=5050)
-
 watson_api_url = _config("WATSON_API_URL", default=None)
 watson_api_key = _config("WATSON_API_KEY", default=None)
 watson_env_id = _config("WATSON_ENV_ID", default=None)
